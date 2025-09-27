36b11c5f96ba268077210fa97353fca28e4ba506
Kim <wxk220007@utdallas.edu>
2025-09-27 18:43:22 -0500

    4 bit versions

---
+++ verilog-practice.srcs/sources_1/new/4bit_adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/27/2025 06:20:39 PM
+// Design Name: 
+// Module Name: 4bit_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module s4bit_adder(
+
+    );
+endmodule
---
+++ verilog-practice.srcs/sources_1/new/4bit_comparator.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/27/2025 06:20:39 PM
+// Design Name: 
+// Module Name: 4bit_comparator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module s4bit_comparator(
+    input [3:0] a,
+    input [3:0] b,
+    input [3:0] sum,
+    output cout
+    );
+endmodule
