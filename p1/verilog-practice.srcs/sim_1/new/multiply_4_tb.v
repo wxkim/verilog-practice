ced5e0fd57cb17ca11adfd77c49ebf0bf7e10be7
Will Kim <will.kim0526@gmail.com>
2025-11-28 17:22:42 -0600

    p2

---
+++ p1/verilog-practice.srcs/sim_1/new/multiply_4_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/28/2025 04:27:45 PM
+// Design Name: 
+// Module Name: multiply_4_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiply_4_tb(
+
+    );
+endmodule
---
+++ p1/verilog-practice.srcs/sources_1/new/4bit_multiplier.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/28/2025 04:16:52 PM
+// Design Name: 
+// Module Name: 4bit_multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiplier_4b(
+    input [3:0] a,
+    input [3:0] b,
+    output [3:0] p
+    );
+endmodule
