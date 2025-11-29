4268eb925ff3b16436050c091e113188a84a380b
Will Kim <will.kim0526@gmail.com>
2025-11-28 23:03:11 -0600

    Hamming 95 enc

--- p2/p2.srcs/sources_1/new/hamming5_encoder.v
+++ p2/p2.srcs/sources_1/new/hamming5_encoder.v
@@ -20,7 +20,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module hamming5_encoder(
-
+module hamming95_encoder(
+    input [4:0] data_in,
+    output [8:0] encoded_out
     );
+    
+    
+    wire p1, p2, p4, p8;
+    
+    wire d1, d2, d3, d4, d5;
+
+    assign d1 = data_in[0];
+    assign d2 = data_in[1];
+    assign d3 = data_in[2];
+    assign d4 = data_in[3];
+    assign d5 = data_in[4];
+
+    
+    assign p1 = d1 ^ d2 ^ d4 ^ d5; 
+    assign p2 = d1 ^ d3 ^ d4;
+    assign p4 = d2 ^ d3 ^ d4;
+    assign p8 = d5;
+    
+    assign encoded_out = {
+        d5,
+        p8,
+        d4,
+        d3,
+        d2,
+        p4,
+        d1,
+        p2,
+        p1
+    };
+    
 endmodule
